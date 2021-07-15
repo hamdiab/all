1f15eba7e48462c9b80683757833e30bda949ed5
hamdiab <87267948+hamdiab@users.noreply.github.com>
2021-07-15 13:06:13 +0300

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,22 @@
+<?
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_form = 'info@yourwebsite.com';
+$email_subject = 'New Form Submission';
+$email_body = "User Name: $name.\n".
+                "User Name: $visitor_email.\n".
+                    "Subject: $subject.\n".
+                    "User Message: $message.\n";
+
+$to = 'humdialhuraibib@gmail.com';
+$headers = "From: $email_form \r\n";
+$headers .= "Reply-to: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+?>
